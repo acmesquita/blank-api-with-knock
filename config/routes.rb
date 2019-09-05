0f379037c03491891c8a25d1da6424af06b802c2
Catharina Mesquita <anacatharina@infoway-pi.com.br>
2019-09-05 12:40:29 -0300

    Api com autenticação usando knock e jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root   'home#index'
+  get    'auth'            => 'home#auth'
+
+  post   'user_token'      => 'user_token#create'
+  
+  get    '/users'          => 'user#index'
+  get    '/users/current'  => 'users#current'
+  post   '/users/create'   => 'users#create'
+  patch  '/user/:id'       => 'users#update'
+  delete '/user/:id'       => 'users#destroy'
 end
